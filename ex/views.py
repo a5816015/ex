b46ac78250dcf35f3b7715af43a1114f7c19683f
a5816074 <miimidayo@gmail.com>
2018-07-23 16:46:00 +0900

    base HTML&CSS

--- ex/views.py
+++ ex/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
+import random
 
 # Create your views here.
 
 def appmain(request):
-	return render(request, 'aaa.html', {})
+	num = random.randint(1,2)
+	path = "silhouette/poke" + str(num) + ".png"
+	
+	f = open('ex/answer.txt', encoding='utf-8')
+	for i in range(1,num+1):
+		answer = f.readline()
+		hint = f.readline()
+	f.close
+	
+	return render(request, 'aaa.html', {
+		'path' : path,
+		'hint' : hint,
+	})
